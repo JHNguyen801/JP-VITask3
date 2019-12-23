68b79b9c35055a5b61a016165d199f2ddf6ddd74
JHNguyen801 <32757548+JHNguyen801@users.noreply.github.com>
2019-12-23 13:19:12 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import DataStreamer, { ServerRespond } from './DataStreamer';
+import Graph from './Graph';
+import './App.css';
+
+interface IState {
+  data: ServerRespond[],
+  showGraph: boolean,
+}
+
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      // data saves the server responds.
+      data: [],
+      showGraph: false,
+    };
+  }
+ 
+  // render graph method
+  renderGraph() {
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
+  }
+ 
+  // get data from the server continuously
+  getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Bank Merge & Co Task 3
+        </header>
+        <div className="App-content">
+          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
+          <div className="Graph">
+            {this.renderGraph()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
